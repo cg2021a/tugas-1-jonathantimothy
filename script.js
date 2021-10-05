e91d5065454f04e9b5d480a8aaf04a0498a963d4
jonathantimothy <90582800+jonathantimothy@users.noreply.github.com>
2021-10-06 06:25:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function main() {
+    var canvas = document.getElementById("myCanvas");
+    var gl = canvas.getContext("webgl");
 
+  
+   var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
